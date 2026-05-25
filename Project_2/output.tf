e1c99d2156c79de1c08d4d01e25eafa37461e939
Jason Sefia <onaefe6@gmail.com>
2026-05-25 22:05:23 +0100

    fix:fmt all files

--- Project_2/output.tf
+++ Project_2/output.tf
@@ -1,19 +1,30 @@
 #Use this to reference VPC ID and Subnet IDs for other modules
 output "vpc-id" {
   description = "vpc ID"
-  value       = module.vpc.vpc-id
+  value = module.vpc.vpc-id
 }
 
 output "private-id" {
   description = "private subnet ID"
-  value       = module.vpc.private-id
+  value = module.vpc.private-id
 }
 
 output "public-id" {
   description = "public subnet ID"
-  value       = module.vpc.public-id
+  value = module.vpc.public-id
 }
 
 output "public-id_2" {
   value = module.vpc.public-id_2
 }
+
+output "kms-key" {
+  description = "kms key for S3 bucket"
+  value = aws_kms_key.kms-key.arn
+}
+
+output "kms-alias-key" {
+  description = "kms key for S3 bucket"
+  value = aws_kms_alias.kms-alias.arn
+}
+
